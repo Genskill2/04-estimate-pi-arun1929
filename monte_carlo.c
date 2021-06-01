1d158937473583c6f172a0ff7936f17342392948
arun1929 <84227806+arun1929@users.noreply.github.com>
2021-06-01 05:29:45 -0400

    Add files via upload

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,5 +38,22 @@ int main(void) {
     }
   }
 }
+float mc_pi(int count)
+{
+	int i;
+	float circount=0;
+	for(i=0;i<count;i++)
+	{
+		float x=frandom();
+		float y=frandom();
+		if(sqrt(x*x+y*y)<1)
+		{
+			circount++;
+		}
+	}
+	float area=((4*circount)/count);
+	return area;
+	
+}
 
 
